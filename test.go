3ec0ff0d0c358e185668ea48fd85a3d98f47e4f8
sophialixiaofei_i <sophialixiaofei_i@didichuxing.com>
2018-08-08 23:34:28 +0800

    first commit

---
+++ test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func worker(id int, c chan int) {
+	for n := range c {
+		fmt.Printf("worker %d received %c \n", id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
+func chanDemo() {
+	var channels [10] chan<- int
+	for i := 0; i < 10; i++ {
+		channels[i] = createWorker(i)
+	}
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'a' + i
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'A' + i
+	}
+
+	time.Sleep(time.Millisecond)
+}
+
+func main() {
+	chanDemo()
+
+}
